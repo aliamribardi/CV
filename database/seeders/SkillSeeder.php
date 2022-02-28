f4273057d11dbf6d1891e55caf1b08631dbc0cd6
aliamribardi <aliamribardi1@gmail.com>
2022-03-01 00:26:29 +0800

    seeder

--- database/seeders/SkillSeeder.php
+++ database/seeders/SkillSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Skill;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class SkillSeeder extends Seeder
 {
