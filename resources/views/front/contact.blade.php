374d02d00a72e2b70ff9d7519f5eed1866a65ef7
aliamribardi <aliamribardi1@gmail.com>
2022-03-01 20:46:58 +0800

    contact

--- resources/views/front/contact.blade.php
+++ resources/views/front/contact.blade.php
@@ -1,24 +1,5 @@
 <section id="contact">
 
-        <div class="row section-head">
-
-        <div class="two columns header-col">
-
-            <h1><span>Get In Touch.</span></h1>
-
-        </div>
-
-        <div class="ten columns">
-
-                <p class="lead">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-                eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
-                voluptatem quia voluptas sit aspernatur aut odit aut fugit.
-                </p>
-
-        </div>
-
-        </div>
-
         <div class="row">
 
         <div class="eight columns">
@@ -73,38 +54,13 @@
 
                     <h4>Address and Phone</h4>
                     <p class="address">
-                        Jonathan Doe<br>
-                        1600 Amphitheatre Parkway <br>
-                        Mountain View, CA 94043 US<br>
-                        <span>(123) 456-7890</span>
+                        {{ $banners->name ?? 'No Data' }}<br>
+                        {{ $abouts->address ?? 'No Data' }}<br>
+                        <span>{{ $abouts->phone_number ?? 'No Data' }}</span>
                     </p>
 
                 </div>
 
-            <div class="widget widget_tweets">
-
-                <h4 class="widget-title">Latest Tweets</h4>
-
-                <ul id="twitter">
-                    <li>
-                    <span>
-                    This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
-                    Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum
-                    <a href="#">http://t.co/CGIrdxIlI3</a>
-                    </span>
-                    <b><a href="#">2 Days Ago</a></b>
-                    </li>
-                    <li>
-                    <span>
-                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-                    eaque ipsa quae ab illo inventore veritatis et quasi
-                    <a href="#">http://t.co/CGIrdxIlI3</a>
-                    </span>
-                    <b><a href="#">3 Days Ago</a></b>
-                    </li>
-                </ul>
-
-                </div>
 
         </aside>
 
