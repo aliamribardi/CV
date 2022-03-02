3c8d95d93c0d880f43d0b0fa5160d46f1da40576
aliamribardi <aliamribardi1@gmail.com>
2022-03-02 15:58:18 +0800

    sosmed

--- resources/views/front/layouts/banner.blade.php
+++ resources/views/front/layouts/banner.blade.php
@@ -6,13 +6,13 @@
     and learn more <a class="smoothscroll" href="#about">about me</a>.</h3> --}}
     <hr />
     <ul class="social">
-        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-        <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-        <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-        <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-        <li><a href="#"><i class="fa fa-skype"></i></a></li>
+        <li><a href="{{ $sosmed->facebook }}" target="__blank"><i class="fa fa-facebook"></i></a></li>
+        <li><a href="{{ $sosmed->twitter }}" target="__blank"><i class="fa fa-twitter"></i></a></li>
+        <li><a href="{{ $sosmed->google_plus }}" target="__blank"><i class="fa fa-google-plus"></i></a></li>
+        <li><a href="{{ $sosmed->linkedin }}" target="__blank"><i class="fa fa-linkedin"></i></a></li>
+        <li><a href="{{ $sosmed->instagram }}" target="__blank"><i class="fa fa-instagram"></i></a></li>
+        <li><a href="{{ $sosmed->dribbblle }}" target="__blank"><i class="fa fa-dribbble"></i></a></li>
+        <li><a href="{{ $sosmed->facebook }}" target="__blank"skype><i class="fa fa-skype"></i></a></li>
     </ul>
     </div>
 </div>
